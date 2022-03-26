233eb4af1105497b70fe76fa12cd5d73db6b5ab9
pg4devt <pg4devt@gmail.com>
2022-03-26 10:26:30 +0800

    Initial for angular frontend and dotnet backend

---
+++ Northwind/Northwind.Backend/DataContext/NorthwindContext.cs
@@ -0,0 +1,15 @@
+﻿using BackEnd.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace BackEnd.DataContext
+{
+    public class NorthwindContext : DbContext
+    {
+        public NorthwindContext(DbContextOptions options)
+            : base(options)
+        {
+        }
+
+        public virtual DbSet<Employee> Employees { get; set; } = null!;
+    }
+}
---
+++ Northwind/Northwind.Backend/Program.cs
@@ -0,0 +1,35 @@
+using BackEnd.DataContext;
+using Microsoft.EntityFrameworkCore;
+using System.Configuration;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+
+builder.Services.AddControllers();
+builder.Services.AddDbContext<NorthwindContext>(options =>
+{
+    var conStr = builder.Configuration.GetConnectionString("NorthwindDatabase");
+    options.UseSqlServer(conStr);
+});
+
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+    app.UseDeveloperExceptionPage();
+}
+
+app.UseHttpsRedirection();
+
+app.UseAuthorization();
+
+app.MapControllers();
+
+app.Run();
